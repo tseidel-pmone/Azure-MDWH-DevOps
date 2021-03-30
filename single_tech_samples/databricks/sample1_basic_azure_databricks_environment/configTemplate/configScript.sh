efa558e496365cf87b816152cda6705e0401c235
juan <jsburckhardt@gmail.com>
2021-03-30 17:26:24 +1100

    feat(sample1-deploy-batch): test deploy to azure button with script

--- single_tech_samples/databricks/sample1_basic_azure_databricks_environment/configTemplate/configScript.sh
+++ single_tech_samples/databricks/sample1_basic_azure_databricks_environment/configTemplate/configScript.sh
@@ -10,6 +10,7 @@ DEPLOY_PRINCIPAL_ID=${DEPLOY_PRINCIPAL_ID:-}
 keyVaultName="${DEPLOYMENT_PREFIX}akv01"
 storageAccountName="${DEPLOYMENT_PREFIX}asa01"
 adbWorkspaceName="${DEPLOYMENT_PREFIX}adb01"
+scopeName="storage_scope"
 
 echo "Retrieving keys from storage account"
 storageKeys=$(az storage account keys list --resource-group "$AZURE_RESOURCE_GROUP_NAME" --account-name "$storageAccountName")
